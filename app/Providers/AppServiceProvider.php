4e445ac6848bfdbec9825d4beb5260080eecaaa5
Edgar Cano <garicano25@gmail.com>
2025-04-24 00:36:17 -0600

    Get List Menu - Routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        View::composer('*', function ($view) {
+            if (Auth::check()) {
+                $user = Auth::id();
+                $views = DB::select("
+                SELECT v.ruta AS RUTA, v.nombre as NOMBRE, v.icono AS ICONO
+                FROM views_gym vg
+                LEFT JOIN users u ON u.id = vg.gym_id
+                LEFT JOIN views v ON v.id = vg.view_id
+                WHERE vg.gym_id = ? AND v.status_id = 1
+            ", [$user]);
+
+                $view->with('views', $views);
+            }
+        });
     }
 }
